4dc75b00c54d8ec23bfe4706ea875cd709364630
Ramana KS <ksr23110367@gmail.com>
2026-06-16 17:07:57 +0530

    feat: add basic hero page

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,25 @@
-import './App.css'
+// App component
 
-const App = () => {
+import { useState } from 'react';
+import './App.css'
+import Search from './components/Search';
+
+export default function App() {
+  const [searchTerm, setSearchTerm] = useState('');
+
   return (
     <main>
       <div className="pattern"/>
 
       <div className='wrapper'>
         <header>
+          <img src='hero.png' alt='hero-banner'/>
           <h1>Find <span className='text-gradient'>Movies</span> That Resonate With You</h1>
         </header>
+
+        <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm}/>
       </div>
     </main>
   );
 }
 
-export default App
---
+++ src/components/Search.jsx
@@ -0,0 +1,21 @@
+// Search bar
+
+export default function Search({ searchTerm, setSearchTerm }) {
+    return (
+        <div className="search">
+            <div>
+                <img src="search.svg"/>
+
+                <input 
+                    type="text"
+                    placeholder="Dive into 1000+ movies"
+                    value={searchTerm}
+                    onChange={(e) => {setSearchTerm(e.target.value)}}
+                />
+            </div>
+            <h3 className="text-white">
+                    {searchTerm}
+                </h3>
+        </div>
+    );
+}
